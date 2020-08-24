254ec4becffceaa43f918447b7f5bca95c645dea
Oliver Marco van Komen <olvan18@student.sdu.dk>
2020-08-24 12:27:05 +0200

    Database Forecast models

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -5,7 +5,9 @@ namespace Server.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Subscription> Subscriptions { get; set; }
+        public DbSet<CurrentConditionSubscription> CurrentConditionSubscriptions { get; set; }
+        public DbSet<ForecastSubscription> ForecastSubscriptions { get; set; }
     }
 }
