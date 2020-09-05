8de8a7239326a36386d79e61b60144dbeba8fce9
Oliver Marco van Komen <olvan18@student.sdu.dk>
2020-09-05 11:11:37 +0200

    Database forecast update

---
+++ Server/Migrations/20200905090954_Forecast.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Server.Migrations
+{
+    public partial class Forecast : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GeoCode",
+                table: "Subscriptions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Subscriptions",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GeoCode",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Subscriptions");
+        }
+    }
+}
